44fd8d1dc96acc928b61ef88e5e351302215a93c
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 16:45:14 -0500

    game controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,3 +10,15 @@ get '/available_decks' do
   @decks = Deck.get_all_decks
 erb :am_test
 end
+
+get '/game/:deck_id' do
+  @deck = Deck.find_by_id(params[:deck_id])
+  current_card = params[:card_id].to_i
+  @cards = (Card.get_cards_by_deck(@deck.id))[current_card]
+  erb :game
+end
+
+# post '/submit_answer' do
+#   guess = params[:guess]
+#   Guess.enter_answer(guess)
+# end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,8 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
+
+  def self.get_cards_by_deck(deck_id)
+    self.where("deck_id = #{deck_id}")
+
+  end
 end
