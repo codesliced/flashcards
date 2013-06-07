e746f8fcb6eeb9dd3436c5ad2a5f283e514c8a0b
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 14:59:33 -0500

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,10 @@ require 'csv'
 files = ["drivered", "movies", "pop_music", "videogames"]
 
 files.each do |file|
+  current_deck = Deck.find_or_create_by_name(file)
   CSV.foreach(File.expand_path("db/csv/#{file}.csv"), 'r') do |line|
     begin
-      Card.new
+      current_deck.cards << Card.create(front: line[0], back: line[1])
     rescue
       puts "problem"
     end
