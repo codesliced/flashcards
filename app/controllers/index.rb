6e61ec3ff1b0ac8e846090015cfa356989832850
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 20:07:11 -0500

    so much shit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,12 +12,21 @@ get '/available_decks' do
 erb :am_test
 end
 
-get '/game/:deck_id' do
+get '/game/:round_id/:deck_id' do
   @deck = Deck.find_by_id(params[:deck_id])
   @current_card = params[:card_id].to_i
-  @cards = (Card.get_cards_by_deck(@deck.id))[@current_card]
+  @card = (Card.get_cards_by_deck(@deck.id))[@current_card]
   @game_over = @current_card >= (@deck.cards.count - 1)
-  erb :game
+  @card_side = params[:card_side]
+  @round_id = params[:round_id]
+  erb :card
+end
+
+post '/new_card/:answer' do
+  answer = params[:answer]
+  route = params[:route]
+
+  redirect to route
 end
 
 get '/user/:id' do
@@ -25,6 +34,12 @@ get '/user/:id' do
 erb :user_profile
 end
 
+
+get '/initiate_new_game' do
+  round = Round.create(:deck_id => params[:id])
+
+  redirect to "/game/#{round.id}/#{params[:id]}"
+end
 
 # post '/submit_answer' do
 #   guess = params[:guess]
